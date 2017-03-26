a2cca182ba7d84cc725dd390b17e9d033212d7a0
Ziv Shtaeinberg <zivshtaeinberg@gmail.com>
2017-03-26 17:56:56 +0300

    gov il first bot

--- messages/index.js
+++ messages/index.js
@@ -20,21 +20,16 @@ var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
+        builder.Prompts.text(session, "ברוף הבא לGOVIL, מה תעודת הזהות שלך?");
     },
     function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+        session.userData.id = results.response;
+        session.send("שלום זיו שטיינברג"); 
+        builder.Prompts.choice(session, "מה תרצה לעשות?",["תשלום קנסות תעבורה","ספח חדש לתעודת זהות"]);
     },
     function (session, results) {
         session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                    " you've been programming for " + session.userData.coding + 
-                    " years and use " + session.userData.language + ".");
+        session.send("אין בעיה, מתחלים בתהליך" + results.response.entity);
     }
 ]);
 
